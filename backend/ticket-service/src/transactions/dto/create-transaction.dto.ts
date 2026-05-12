957b243e3ccb182c89467f3aa6aa63173fea1d5e
pnuraini100-bit <pnuraini100@gmail.com>
2026-05-12 09:02:15 +0700

    feat: tambah dokumentasi api pakai swagger

--- backend/ticket-service/src/transactions/dto/create-transaction.dto.ts
+++ backend/ticket-service/src/transactions/dto/create-transaction.dto.ts
@@ -1,24 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsString, IsPositive, MinLength, IsOptional } from 'class-validator';
 
 export class CreateTransactionDto {
-  @IsInt()
+  @ApiProperty()
   tiketId!: number;
 
-  @IsInt()
-  @IsPositive()
+  @ApiProperty()
   jumlah!: number;
 
-  @IsString()
-  @MinLength(3)
+  @ApiProperty()
   namaPembeli!: string;
 }
 
 export class UpdateTransactionDto {
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsInt()
   @IsPositive()
   jumlah?: number;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   @MinLength(3)
