e4e7f6e6cc4c34fbf160ba3dfff53dde23eb9f7a
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2026-05-25 10:44:42 +0700

    reset repository

--- backend/ticket-service/prisma/migrations/20260511133752_tambah_kolom_umur_hewan/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `umur` to the `Hewan` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Hewan" ADD COLUMN     "lokasi" TEXT,
-ADD COLUMN     "umur" INTEGER NOT NULL;
