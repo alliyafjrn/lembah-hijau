71c9dcb546c83d68a08dd7ec745cf248199dba8d
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2026-05-26 14:20:09 +0700

    generate prisma module dan prisma service

--- backend/kategori-service/src/app.module.ts
+++ backend/kategori-service/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KategoriModule } from './kategori/kategori.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [KategoriModule],
+  imports: [KategoriModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
