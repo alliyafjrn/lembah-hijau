a874168c963767dd441833cee832b3fa12e49557
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2026-05-28 22:28:40 +0700

    install dan setup swagger documentation

--- backend/kategori-service/src/main.ts
+++ backend/kategori-service/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Kategori Service API')
+    .setDescription('API kategori wisata lembah hijau')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
