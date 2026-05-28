c056e80b99d9a7f4cf34294295748613eb1e552e
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2026-05-28 20:47:48 +0700

    membuat endpoint post kategori

--- backend/kategori-service/src/kategori/kategori.controller.ts
+++ backend/kategori-service/src/kategori/kategori.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { KategoriService } from './kategori.service';
+import { CreateKategoriDto } from './dto/create-kategori.dto';
 
 @Controller('kategori')
-export class KategoriController {}
+export class KategoriController {
+  constructor(private readonly kategoriService: KategoriService) {}
+
+  @Post()
+  create(@Body() createKategoriDto: CreateKategoriDto) {
+    return this.kategoriService.create(createKategoriDto);
+  }
+}
