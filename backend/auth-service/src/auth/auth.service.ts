f351082bc6cfed7423f4979fbeb34729d0f733a5
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2026-06-04 09:33:54 +0700

    implementasi login user

--- backend/auth-service/src/auth/auth.service.ts
+++ backend/auth-service/src/auth/auth.service.ts
@@ -1,17 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto} from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService) { }
 
-  async register(dto: RegisterDto) {
-    return this.prisma.user.create({
-      data: {
-        email: dto.email,
-        password: dto.password,
-      },
-    });
-  }
+    async register(dto: RegisterDto) {
+        return this.prisma.user.create({
+            data: {
+                email: dto.email,
+                password: dto.password,
+            },
+        });
+    }
+    async login(dto: LoginDto) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            },
+        });
+
+        if (!user) {
+            return {
+                success: false,
+                message: 'User tidak ditemukan',
+            };
+        }
+
+        if (user.password !== dto.password) {
+            return {
+                success: false,
+                message: 'Password salah',
+            };
+        }
+
+        return {
+            success: true,
+            message: 'Login berhasil',
+            user,
+        };
+    }
 }
