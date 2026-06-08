678d8a3867fca32345cba01667c33dd7852fd471
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2026-06-08 17:39:45 +0700

    buat halaman user

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,31 @@
-import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default function Home() {
-  redirect("/dashboard");
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center bg-green-50">
+      <h1 className="text-5xl font-bold text-green-700 mb-4">
+        Lembah Hijau
+      </h1>
+
+      <p className="text-xl text-gray-700 mb-8">
+        Tempat Wisata Keluarga di Bandar Lampung
+      </p>
+
+      <div className="flex gap-4">
+        <Link
+          href="/wisata-user"
+          className="bg-green-700 text-white px-6 py-3 rounded-lg"
+        >
+          Jelajahi Wisata
+        </Link>
+
+        <Link
+          href="/dashboard"
+          className="bg-blue-600 text-white px-6 py-3 rounded-lg"
+        >
+          Admin
+        </Link>
+      </div>
+    </main>
+  );
 }
