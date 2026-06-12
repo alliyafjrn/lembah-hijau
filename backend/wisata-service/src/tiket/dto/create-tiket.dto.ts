026beef8c6982fb7d47d54e86a17c59782c37e70
alliyafjrn <alliya_fajrin@teknokrat.ac.id>
2026-06-12 16:44:35 +0700

    update harga tiket dto

--- backend/wisata-service/src/tiket/dto/create-tiket.dto.ts
+++ backend/wisata-service/src/tiket/dto/create-tiket.dto.ts
@@ -1,37 +1,17 @@
-import { IsNotEmpty, IsString, IsEmail, IsNumber } from 'class-validator';
-import { Min } from 'class-validator'; 
-import { Type } from 'class-transformer';
-
 export class CreateTiketDto {
-  @IsNotEmpty()
-  @IsString()
-  namaPemesan!: string;
 
-  @IsNotEmpty()
-  @IsEmail()
-  email!: string;
+namaPemesan!:string;
 
-  @IsString()
-  @IsNotEmpty()
-  jenisTiket!: string; 
+email!: string;
 
-  
-  @IsNumber()
-  @IsNotEmpty()
-  harga!: number; 
-  
-  @IsNotEmpty()
-  @IsNumber()
-  @Min(1)
-  @Type(() => Number) 
-  jumlahTiket!: number;
+jenisTiket!: string;
 
-  @IsNumber()
-  @IsNotEmpty()
-  totalHarga!: number; 
+harga!: number;
 
-  @IsNumber()
-  @IsNotEmpty()
-  wisataId!: number;
+jumlahTiket!: number;
+
+totalHarga!: number;
+
+wisataId! : number;
 
 }
